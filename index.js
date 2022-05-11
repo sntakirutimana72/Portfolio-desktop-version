ff5fec9aabc84f9f949ed10ce876e55554d8fc48
unknown <sntakirutimana72@gmail.com>
2022-05-11 14:58:41 +0200

    Add ability to load projects section dynamically

--- index.js
+++ index.js
@@ -4,23 +4,38 @@ function loadWizard() {
   const wizard = document.createElement('div');
 
   wizard.classList.add('details-wizard-wrap', 'hidden');
-  wizard.innerHTML = wizardView;
-
-  text(wizard.querySelector('h3'), data[0].name);
-  text(wizard.querySelector('p'), data[0].desc);
-
-  wizard.querySelectorAll('li')
-    .forEach((li, index) => text(li, data[0].technologies[index]));
-  wizard.querySelectorAll('.details-btn-live')
-    .forEach((anchor) => attribute(anchor, 'href', data[0].live));
-  wizard.querySelectorAll('.details-btn-src')
-    .forEach((anchor) => attribute(anchor, 'href', data[0].source));
+  wizard.innerHTML = wizardInterface;
 
   select('main').appendChild(wizard);
 
   isWizardLoaded = true;
 
   return wizard;
+}
+
+function loadAllProjects() {
+  const projContainer = document.createElement('section');
+  projContainer.classList.add('works', 'main-item');
+  attribute(projContainer, 'id', 'portfolio');
+  projContainer.innerHTML = projectContainer;
+
+  const container = projContainer.querySelector('.works-container');
+
+  projectsData.forEach((data) => {
+    const project = document.createElement('div');
+    project.classList.add('works-card');
+    attribute(project, 'data-target', data.id);
+    project.innerHTML = projectCardInterface;
+
+    [project.querySelector('h5').innerHTML] = data.name;
+    project.querySelectorAll('li')
+      .forEach((li, index) => text(li, data.technologies[0][index]));
+
+    container.appendChild(project);
+  });
+
+  const main = document.body.querySelector('main');
+  main.insertBefore(projContainer, main.children[1]);
 }
 
 function toggleOverflow() {
@@ -45,9 +60,7 @@ function toggleDetailsWizard(element) {
   toggleClass(element, 'hidden');
 }
 
-function triggerDetailsWizard(evt) {
-  evt.preventDefault();
-
+function triggerDetailsWizard(target) {
   let wizard;
 
   if (!isWizardLoaded) {
@@ -56,13 +69,33 @@ function triggerDetailsWizard(evt) {
     wizard = select('.details-wizard-wrap');
   }
 
+  const data = projectsData.find((project) => project.id === target);
+
+  text(wizard.querySelector('h3'), data.name[1]);
+  text(wizard.querySelector('p'), data.description);
+
+  wizard.querySelectorAll('img')
+    .forEach((image, index) => attribute(image, 'src', data.image[index]));
+  wizard.querySelectorAll('li')
+    .forEach((li, index) => text(li, data.technologies[1][index]));
+  wizard.querySelectorAll('.details-btn-live')
+    .forEach((anchor) => attribute(anchor, 'href', data.live));
+  wizard.querySelectorAll('.details-btn-src')
+    .forEach((anchor) => attribute(anchor, 'href', data.source));
+
   toggleDetailsWizard(wizard);
 }
 
 window.addEventListener('DOMContentLoaded', () => {
+  loadAllProjects();
+
   document.body.addEventListener('click', (event) => {
     if (event.target.classList.contains('details-x')) {
       toggleDetailsWizard();
+    } else if (event.target.classList.contains('project-btn')) {
+      event.preventDefault();
+      const parent = event.target.parentElement.parentElement.parentElement;
+      triggerDetailsWizard(attribute(parent, 'data-target'));
     }
   });
 
@@ -74,7 +107,4 @@ window.addEventListener('DOMContentLoaded', () => {
     .forEach((node) => {
       node.onclick = toggleMobileMenu;
     });
-
-  selectAll('.card-btn-wrapper a')
-    .forEach((button) => button.addEventListener('click', triggerDetailsWizard));
 });
--- static/js/static.js
+++ static/js/static.js
@@ -1,16 +1,14 @@
-const wizardView = `
+const wizardInterface = `
   <div>
     <div class="details-wizard">
       <div>
           <div class="details-top">
               <button class="details-x btn"></button>
               <img
-                  src="static/images/details-portfolio-873w.png"
                   alt="Details Portfolio Image"
                   class="details-image"
               />
               <img
-                  src="static/images/details-portfolio-311w.png"
                   alt="Details Portfolio Image"
                   class="details-image-mob hidden"
               />
@@ -20,8 +18,8 @@ const wizardView = `
       <div>
           <div class="details-heading-wrap">
               <h3 class="details-proj-heading"></h3>
-              <a class="details-btn details-btn-live btn">See live</a>
-              <a class="details-btn details-btn-src btn">See Source</a>
+              <a class="details-btn details-btn-live btn" target="_blank">See live</a>
+              <a class="details-btn details-btn-src btn" target="_blank">See Source</a>
           </div>
       </div>
 
@@ -39,35 +37,271 @@ const wizardView = `
       </div>
       <p class="details-para"></p>
       <div class="details-ext-refs hidden">
-          <a class="details-btn details-btn-live btn">See live</a>
-          <a class="details-btn details-btn-src btn">See Source</a>
+          <a class="details-btn details-btn-live btn" target="_blank">See live</a>
+          <a class="details-btn details-btn-src btn" target="_blank">See Source</a>
       </div>
     </div>
   </div>
 `;
 
-const data = [
+const projectContainer = `
+  <div class="works-wrapper">
+    <h3 class="works-h2">
+      My Recent Works<br>
+      <span class="hidden"></span>
+    </h3>
+
+    <div>
+      <div class="works-container">
+      </div>
+    </div>
+  </div>
+`;
+
+const projectCardInterface = `
+  <div class="works-card-body">
+      <h5 class="works-card-h5"></h5>
+      <div>
+          <ul class="works-card-list">
+              <li class="works-card-list-item"></li>
+              <li class="works-card-list-item"></li>
+              <li class="works-card-list-item"></li>
+              <li class="works-card-list-item"></li>
+          </ul>
+      </div>
+      <div class="card-btn-wrapper">
+          <a class="project-btn btn" href="#">See Project</a>
+      </div>
+  </div>
+`;
+
+const projectsData = [
   {
-    name: 'Keeping track of hundreds of components',
-    desc: `
+    id: 'proj01',
+    name: [
+      'Multi-Post Stories<br>Gain+Glory',
+      'Keeping track of hundreds of components',
+    ],
+    description: `
       Lorem Ipsum is simply dummy text of the printing and typesetting industry.
       Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
       when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem
       Ipsum is simply dummy text of the printing and typesetting industry.
       Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
       when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
-    image: '',
-    technologies: [
-      'Codekit',
-      'GitHub',
-      'Ruby on rails',
-      'css',
-      'JavaScript',
-      'Bootstrap',
-      'Terminal',
-      'Codepen',
+    image: [
+      'static/images/details-portfolio-873w.png',
+      'static/images/details-portfolio-311w.png',
     ],
-    live: '#',
-    source: '#',
+    technologies: [
+      [
+        'Ruby on rails',
+        'css',
+        'JavaScript',
+        'html',
+      ],
+      [
+        'Codekit',
+        'GitHub',
+        'Ruby on rails',
+        'css',
+        'JavaScript',
+        'Bootstrap',
+        'Terminal',
+        'Codepen',
+      ],
+    ],
+    live: 'https://sntakirutimana72.github.io/Portfolio-desktop-version/',
+    source: 'https://github.com/sntakirutimana72/Portfolio-desktop-version',
+  },
+  {
+    id: 'proj02',
+    name: [
+      'Multi-Post Stories<br>Gain+Glory',
+      'Keeping track of hundreds of components',
+    ],
+    description: `
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+      when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem
+      Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+      when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
+    image: [
+      'static/images/details-portfolio-873w.png',
+      'static/images/details-portfolio-311w.png',
+    ],
+    technologies: [
+      [
+        'Ruby on rails',
+        'css',
+        'JavaScript',
+        'html',
+      ],
+      [
+        'Codekit',
+        'GitHub',
+        'Ruby on rails',
+        'css',
+        'JavaScript',
+        'Bootstrap',
+        'Terminal',
+        'Codepen',
+      ],
+    ],
+    live: 'https://sntakirutimana72.github.io/Portfolio-desktop-version/',
+    source: 'https://github.com/sntakirutimana72/Portfolio-desktop-version',
+  },
+  {
+    id: 'proj03',
+    name: [
+      'Multi-Post Stories<br>Gain+Glory',
+      'Keeping track of hundreds of components',
+    ],
+    description: `
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+      when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem
+      Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+      when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
+    image: [
+      'static/images/details-portfolio-873w.png',
+      'static/images/details-portfolio-311w.png',
+    ],
+    technologies: [
+      [
+        'Ruby on rails',
+        'css',
+        'JavaScript',
+        'html',
+      ],
+      [
+        'Codekit',
+        'GitHub',
+        'Ruby on rails',
+        'css',
+        'JavaScript',
+        'Bootstrap',
+        'Terminal',
+        'Codepen',
+      ],
+    ],
+    live: 'https://sntakirutimana72.github.io/Portfolio-desktop-version/',
+    source: 'https://github.com/sntakirutimana72/Portfolio-desktop-version',
+  },
+  {
+    id: 'proj04',
+    name: [
+      'Multi-Post Stories<br>Gain+Glory',
+      'Keeping track of hundreds of components',
+    ],
+    description: `
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+      when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem
+      Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+      when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
+    image: [
+      'static/images/details-portfolio-873w.png',
+      'static/images/details-portfolio-311w.png',
+    ],
+    technologies: [
+      [
+        'Ruby on rails',
+        'css',
+        'JavaScript',
+        'html',
+      ],
+      [
+        'Codekit',
+        'GitHub',
+        'Ruby on rails',
+        'css',
+        'JavaScript',
+        'Bootstrap',
+        'Terminal',
+        'Codepen',
+      ],
+    ],
+    live: 'https://sntakirutimana72.github.io/Portfolio-desktop-version/',
+    source: 'https://github.com/sntakirutimana72/Portfolio-desktop-version',
+  },
+  {
+    id: 'proj05',
+    name: [
+      'Multi-Post Stories<br>Gain+Glory',
+      'Keeping track of hundreds of components',
+    ],
+    description: `
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+      when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem
+      Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+      when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
+    image: [
+      'static/images/details-portfolio-873w.png',
+      'static/images/details-portfolio-311w.png',
+    ],
+    technologies: [
+      [
+        'Ruby on rails',
+        'css',
+        'JavaScript',
+        'html',
+      ],
+      [
+        'Codekit',
+        'GitHub',
+        'Ruby on rails',
+        'css',
+        'JavaScript',
+        'Bootstrap',
+        'Terminal',
+        'Codepen',
+      ],
+    ],
+    live: 'https://sntakirutimana72.github.io/Portfolio-desktop-version/',
+    source: 'https://github.com/sntakirutimana72/Portfolio-desktop-version',
+  },
+  {
+    id: 'proj06',
+    name: [
+      'Multi-Post Stories<br>Gain+Glory',
+      'Keeping track of hundreds of components',
+    ],
+    description: `
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+      when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem
+      Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+      when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
+    image: [
+      'static/images/details-portfolio-873w.png',
+      'static/images/details-portfolio-311w.png',
+    ],
+    technologies: [
+      [
+        'Ruby on rails',
+        'css',
+        'JavaScript',
+        'html',
+      ],
+      [
+        'Codekit',
+        'GitHub',
+        'Ruby on rails',
+        'css',
+        'JavaScript',
+        'Bootstrap',
+        'Terminal',
+        'Codepen',
+      ],
+    ],
+    live: 'https://sntakirutimana72.github.io/Portfolio-desktop-version/',
+    source: 'https://github.com/sntakirutimana72/Portfolio-desktop-version',
   },
 ];
